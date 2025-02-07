4f8825ba410015040453ff98d17103348083b4a0
Federico Quintieri <fedequinti@gmail.com>
2025-02-07 16:16:47 +0100

    commento nel main riga 9

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 const bnb_router = require("./routes/bnb_routes");
-
+// Ciao sono del main
 app.use(
     cors({
       origin: process.env.URL_FRONT, // Sostituisci con il dominio del tuo frontend
